38b8b1a35d17807da8d16aa3778ff6741586760c
Ceren Şahin <cerennssahinn@gmail.com>
2023-07-04 03:50:51 +0300

    Form Submit Test Fail. Cannot find the problem. App works fine.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+function ItemForm({ onInputChange, onSelectChange, onItemFormSubmit, input, selectCategoryToAdd }) {
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={onInputChange} value={input}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={onSelectChange} value={selectCategoryToAdd}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,26 +2,64 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+  const [input, setInput] = useState("");
+  const [selectCategoryToAdd, setSelectCategoryToAdd] = useState("Produce");
+  const [newItems, setNewItems] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSearch(event.target.value);
+  }
 
-    return item.category === selectedCategory;
-  });
+  function handleInputChange(event) {
+    setInput(event.target.value);
+  }
+
+  function handleSelectChange(event) {
+    setSelectCategoryToAdd(event.target.value);
+  }
+
+
+  function onItemFormSubmit(event) {
+    event.preventDefault();
+     const newItem = {
+      id: uuid(),
+      name: input,
+      category: selectCategoryToAdd,
+    }
+    const itemsToDisplay = [...newItems, newItem];
+
+
+    setNewItems(itemsToDisplay);
+    setInput(""); 
+    setSelectCategoryToAdd("Produce");
+  }
+
+  const updatedItemsToDisplay = newItems
+  .filter((item) => {
+      if (selectedCategory === "All") return true;
+      return item.category === selectedCategory;
+  })
+  .filter((item) => item.name.includes(search))
+  
+
+
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onInputChange={handleInputChange} onSelectChange={handleSelectChange} onItemFormSubmit={onItemFormSubmit} input={input} selectCategoryToAdd={selectCategoryToAdd}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={search}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {updatedItemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
